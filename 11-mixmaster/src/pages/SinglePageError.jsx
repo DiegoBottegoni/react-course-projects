dd35e6fdeb5b2f81fa4edf4b16da699fbeb99397
Diego Bottegoni <146596468+DiegoBottegoni@users.noreply.github.com>
2025-02-12 16:47:54 -0300

    Delete 11-mixmaster directory

--- 11-mixmaster/src/pages/SinglePageError.jsx
+++
@@ -1,7 +0,0 @@
-import { useRouteError } from "react-router-dom";
-
-const SinglePageError = () => {
-  const error = useRouteError();
-  return <div>{error.message}</div>;
-};
-export default SinglePageError;
